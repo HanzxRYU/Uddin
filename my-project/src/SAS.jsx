93a8f3b705b08933c9c84942edeb285630058e7a
HanzxRYU <rafi0315@abudzar.sch.id>
2025-06-05 11:13:08 +0700

    adjustment

--- my-project/src/SAS.jsx
+++ my-project/src/SAS.jsx
@@ -3,33 +3,45 @@ import React from "react";
 
 function SAS() {
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-start p-6 space-y-12">
+    <div className="flex flex-col items-center justify-center h-full p-4 m-4 gap-4 w-96 min-h-screen mx-auto text-center dark:text-white dark:bg-gray-800 bg-gray-100 border-gray-200 rounded-lg shadow-lg max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
       {/* Section 1 */}
-      <section className="w-full max-w-3xl flex flex-col items-center">
-        <h1 className="text-3xl font-bold text-gray-800 mb-6">Aku Suka Lorem </h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, id
-          voluptates optio voluptate eligendi nesciunt corrupti praesentium
-          dignissimos voluptatum fugit iste maxime excepturi, reiciendis, iure
-          deserunt quaerat. Tenetur debitis ex sit pariatur perspiciatis natus
-          dicta ad incidunt impedit. Fugit porro, atque culpa ad consequuntur
-          ipsam quasi optio cumque. Error aliquam officiis eaque accusamus
-          incidunt ullam minus tempora! Ab, atque excepturi rem dignissimos
-          impedit cumque beatae error ipsa consequatur vel, hic facere nisi
-          temporibus perferendis soluta repellendus rerum reprehenderit libero
-          dolorem nulla consequuntur voluptatem a? Totam, officia dignissimos
-          quaerat explicabo reiciendis deleniti quasi repellendus cumque
-          molestiae pariatur non nam sequi eligendi voluptates temporibus
-          delectus provident, aliquam, molestias dolor exercitationem neque!
-          Possimus commodi aspernatur sit eveniet neque quaerat nostrum ab
-          numquam sequi!
+      <a href="#">
+        <img class="rounded-t-lg" src="SUKI.jpg" alt="TS PMO" />
+      </a>
+      <div class="p-5">
+        <a href="#">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            MAS IRONI BERCERITA
+          </h5>
+        </a>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet
+          similique expedita ratione, sapiente voluptas est ipsa corporis quos!
+          Delectus nostrum ex in at sapiente repudiandae, est architecto iusto?
+          
         </p>
-        <img
-          src="UIAAAUIA.gif"
-          alt="Collaboration project"
-          className="rounded-2xl w-18 h-18"
-        />
-      </section>
+        <a
+          href="#"
+          class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Read more
+          <svg
+            class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 10"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M1 5h12m0 0L9 1m4 4L9 9"
+            />
+          </svg>
+        </a>
+      </div>
     </div>
   );
 }
