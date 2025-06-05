a045896c97d426d9d4b3976bd83938afefed34d7
HanzxRYU <rafi0315@abudzar.sch.id>
2025-06-05 10:52:58 +0700

    add title, paragraph and pict

---
+++ my-project/src/SAS.jsx
@@ -0,0 +1,37 @@
+// File: App.jsx
+import React from "react";
+
+function SAS() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-start p-6 space-y-12">
+      {/* Section 1 */}
+      <section className="w-full max-w-3xl flex flex-col items-center">
+        <h1 className="text-3xl font-bold text-gray-800 mb-6">Aku Suka Lorem </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, id
+          voluptates optio voluptate eligendi nesciunt corrupti praesentium
+          dignissimos voluptatum fugit iste maxime excepturi, reiciendis, iure
+          deserunt quaerat. Tenetur debitis ex sit pariatur perspiciatis natus
+          dicta ad incidunt impedit. Fugit porro, atque culpa ad consequuntur
+          ipsam quasi optio cumque. Error aliquam officiis eaque accusamus
+          incidunt ullam minus tempora! Ab, atque excepturi rem dignissimos
+          impedit cumque beatae error ipsa consequatur vel, hic facere nisi
+          temporibus perferendis soluta repellendus rerum reprehenderit libero
+          dolorem nulla consequuntur voluptatem a? Totam, officia dignissimos
+          quaerat explicabo reiciendis deleniti quasi repellendus cumque
+          molestiae pariatur non nam sequi eligendi voluptates temporibus
+          delectus provident, aliquam, molestias dolor exercitationem neque!
+          Possimus commodi aspernatur sit eveniet neque quaerat nostrum ab
+          numquam sequi!
+        </p>
+        <img
+          src="UIAAAUIA.gif"
+          alt="Collaboration project"
+          className="rounded-2xl w-18 h-18"
+        />
+      </section>
+    </div>
+  );
+}
+
+export default SAS;
